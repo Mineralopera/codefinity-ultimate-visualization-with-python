aad97b197bcfcb5621f790d732ed9e17cd63b9e8
Mineralopera <>
2025-12-16 12:11:13 +0000

    Grouped Bar Chart

---
+++ creating_commonly_used_plots/grouped_bar_chart/main.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+questions = ['question_1', 'question_2', 'question_3']
+positions = np.arange(len(questions))
+
+yes_answers = np.array([500, 240, 726])
+no_answers = np.array([432, 618, 101])
+answers = np.array([yes_answers, no_answers])
+
+width = 0.3
+
+# Iterate over the indices of the correct array
+for i in range(len(answers)):
+    # Create bars for each category with the correct arguments
+    plt.bar(positions + width * i, answers[i], width)
+  
+plt.xticks(positions + width * (len(answers) - 1) / 2, questions)
+
+plt.show()
+
+anwsers_df = pd.DataFrame(answers)
+
+answers_df.plot(kind='bar', title='good')
+plt.show()
